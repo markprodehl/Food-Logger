769842d530bd3809b76ccbf57a9a340251555c82
Mark Prodehl <markprodehl@gmail.com>
2020-04-16 19:03:00 -0700

    Rendering index.handlebars

--- server.js
+++ server.js
@@ -4,17 +4,15 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Routes
 app.get("/", (req, res) => {
     //index id the Handlebars file name.
     res.render("index");
 });
 
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, () => {
+
     console.log("Server listening on: http://localhost:" + PORT);
 });
