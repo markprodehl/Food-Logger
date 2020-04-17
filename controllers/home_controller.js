447366d780b26346130ee74feacceccf6bcf9ccf
Mark Prodehl <markprodehl@gmail.com>
2020-04-17 00:47:29 -0700

    index.handlebards renders burgers - no styling

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,11 +2,16 @@ const router = require("express").Router();
 const burger = require("../models/burger")
 
 router.get("/", (req, res) => {
-    burger.all((burgers) => {
+    burger.all((data) => {
+        const burgerObject = {
+            burgers: data,
+        }
+        console.log(burgerObject);
+        res.render("index", burgerObject);
 
         // This is a temp json dump
-        const templateData = { burgers: JSON.stringify(burgers, null, 2) };
-        res.render("index", templateData);
+        // const templateData = { burgers: JSON.stringify(burgers, null, 2) };
+        // res.render("index", templateData);
 
     })
 });
